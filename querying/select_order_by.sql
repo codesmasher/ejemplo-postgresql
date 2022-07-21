838e6cbd4a3b66033e40b4978fe46842b8196db4
Enrique Sotelo Ponce <sotelo.enrique@gmail.com>
2022-07-20 23:44:45 -0500

    Ejemplos - Select con order by

---
+++ querying/select_order_by.sql
@@ -0,0 +1,12 @@
+﻿-- Consultando y ordenando datos de los clientes
+SELECT
+    length(first_name) AS len,
+    first_name,
+    last_name,
+    email
+FROM
+    customer
+ORDER BY
+    len DESC,
+    first_name,
+    last_name DESC;
